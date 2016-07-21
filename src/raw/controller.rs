7b718a60d912e9c58eac57a5213456d3bd0bd9c7
panicbit <panicbit.dev@gmail.com>
2016-07-21 06:22:36 +0200

    Fix derp

--- src/raw/controller.rs
+++ src/raw/controller.rs
@@ -2,12 +2,8 @@ use super::Frame;
 
 pub enum Controller {}
 
-pub fn lm_controller_new() -> *mut Controller {
-    unimplemented!();
-}
-
 extern {
-    // pub fn lm_controller_new() -> *mut Controller;
+    pub fn lm_controller_new() -> *mut Controller;
     pub fn lm_controller_is_connected(this: *mut Controller) -> bool;
     pub fn lm_controller_delete(this: *mut Controller);
     pub fn lm_controller_frame(this: *mut Controller) -> *mut Frame;
